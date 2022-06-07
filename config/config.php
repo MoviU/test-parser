eaa9827780745c71c5fb5a3db0fcf26ec6e20588
MoviU <mkachimov@gmal.com>
2022-06-07 11:09:12 +0200

    add foreign keys to db

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_NAME', "");
-define('DB_USER', "");
-define('DB_PASS', "");
-define('DB_HOST', "");
+define('DB_NAME', "test_parser");
+define('DB_USER', "root");
+define('DB_PASS', "root");
+define('DB_HOST', "localhost");
 define('CONTACT_MAIL', "example@gmail.com");
 define('MAIL_FROM', "test@gmail.com");
 define('LOG_DIR', "logs");
